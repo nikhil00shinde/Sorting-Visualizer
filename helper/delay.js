a8afb8d540a7efa33d4334d0f184a94b37437c10
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-08-08 19:16:49 +0530

    quickSort fixed

--- helper/delay.js
+++ helper/delay.js
@@ -5,27 +5,27 @@ function delay(time) {
 }
 
 async function sortDone(){
-  for(let j=start;j<end;j++){
+  for(let j=1;j<size;j++){
     let div1 = container.querySelector(`[data-id${arr[j-1].index}='${arr[j-1].id}']`)
     let div2 = container.querySelector(`[data-id${arr[j].index}='${arr[j].id}']`);
-    await delay(2)
-    div1.classList.remove("redC")
-    div1.classList.add("greenC")
-    await delay(3)
-    div2.classList.add("redC")
+    await delay(2);
+    div1.classList.remove("redC");
+    div1.classList.add("greenC");
+    await delay(3);
+    div2.classList.add("redC");
     if(j == size -1){
      div2.classList.remove("redC")
     div2.classList.add("greenC")
     }
    }
-   await delay(50)
+   await delay(50);
 }
 
 
 async function white(){
-    for(let i=start;i<end;i++){
-     let div = container.querySelector(`[data-id${arr[i].index}='${arr[i].id}']`)
-     div.classList.remove("greenC")
+    for(let i=0;i<size;i++){
+     let div = container.querySelector(`[data-id${arr[i].index}='${arr[i].id}']`);
+     div.classList.remove("greenC");
       await delay(15)
     }
 }
